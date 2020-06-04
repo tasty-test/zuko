facaead0601345d8509ce5f4206203c9010b27a5
sani haq <work.sanihaq@gmail.com>
2020-06-04 23:26:20 +0530

    changes name command to mod for src entry file

--- zuko.ts
+++ zuko.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S deno install --allow-net --allow-read -allow-write --allow-env
 import { red } from "https://deno.land/std/fmt/colors.ts";
-import { options } from "./src/commands.ts";
+import { options } from "./src/mod.ts";
 if (import.meta.main && Deno.args.length > 0) {
   if (options.debug) {
     console.log(`${Deno.args[0]}:`, options, Deno.args);
